f5e576f822b1ca6a8e42b8365e004b06237db177
Tanay Tummalapalli <ttanay100@gmail.com>
2023-11-02 00:10:21 +0530

    Better test results

--- test.sh
+++ test.sh
@@ -3,16 +3,22 @@
 # make the binary before testing it
 g++ main.cpp -o bfi
 
-function output {
+function print_result {
     printf '%s %-30s\n' "$1" "$2"
 }
 function test_case {
     output="$(echo "$2"| ./bfi)"
-    echo "$output"
+    exit_code=`echo $?`
     if [[ $output == $3 ]]; then
-        output "$1" "PASS"
+        print_result "$1" "PASS"
+    elif [[ $exit_code != "0"  ]]; then
+        err_msg="ERROR: "
+        err_msg+="$exit_code"
+        print_result "$1" "$err_msg"
     else
-        output "$1" "FAIL"
+        status="FAIL; Actual output: '"
+        status+="$output'"
+        print_result "$1" "$status"
     fi
 }
 
@@ -25,3 +31,4 @@ test_case "prev" "+++++++++++++++++++++++++++++++++++++++++++++++++>++++++++++++
 test_case "printable_loop" "++>+++++[<+>-]++++++++[<++++++>-]<." "7"
 test_case "nested_loops" "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++." "Hello World!"
 test_case "bug-finding hello world" ">[-]<[-]++++++++[->+++++++++<]>.----[--<+++>]<-.+++++++.><.+++.[-][[-]>[-]+++++++++[<+++++>-]<+...--------------.>++++++++++[<+++++>-]<.+++.-------.>+++++++++[<----->-]<.-.>++++++++[<+++++++>-]<++.-----------.--.-----------.+++++++.----.++++++++++++++.>++++++++++[<----->-]<..[-]++++++++++.[-]+++++++[.,]-]" "Hello"
+test_case "hello world code golf" "+[-->-[>>+>-----<<]<--<---]>-.>>>+.>>..+++[.>]<<<<.+++.------.<<-.>>>>+." "Hello, World!"
